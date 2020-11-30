157c72ecba57aad52a2b1f850479d2f1a47a47a5
Thomas Berghold-Wieser <thomas.berghold-wieser@tieto.com>
2020-11-30 12:04:02 +0100

    added designtime context

---
+++ src/WeirdFlex.Data.EF/DesignTimeFlexContextFactory.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeirdFlex.Data.EF
+{
+    public class DesignTimeFlexContextFactory : IDesignTimeDbContextFactory<FlexContext>
+    {
+        /// <summary>
+        /// Creates a new instance of a pas database context used for local development.
+        ///
+        /// This context will be used for "dotnet ef" commands.
+        /// </summary>
+        /// <param name="args">Arguments provided by the design-time service.</param>
+        /// <returns>An instance of <see cref="PasDbContext" />.</returns>
+        public FlexContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<FlexContext>()
+                .UseMySql("Server=localhost;Database=flexDb;User=root;Password=f2S-HtaPD_Lx;")
+                .EnableSensitiveDataLogging();
+
+            return new FlexContext(optionsBuilder.Options);
+        }
+    }
+}
